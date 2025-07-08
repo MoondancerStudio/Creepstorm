82200833d5a8345f02111b1e7e467ee4da65c53a
Mark Kalinovits <kalee.mark@gmail.com>
2025-07-08 23:38:29 +0200

    Only a built tower does damage

--- scripts/TowerSlot.gd
+++ scripts/TowerSlot.gd
@@ -18,7 +18,7 @@ func buildTower() -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Character:
+	if body is Character and hasTower:
 		var character := body as Character
 		character.damage(1)
 	
